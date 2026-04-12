21a788f650ec3975a169f5dc12c3254d4889534b
HeWhoCooked <bintangstudent972@gmail.com>
2026-04-12 13:26:28 +0700

    Add hitungBiaya method to calculate transaction cost

--- src/logic/Transaksi.java
+++ src/logic/Transaksi.java
@@ -3,11 +3,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package logic;
-
+import model.Tiket;
+import java.time.Duration;
+import java.time.LocalDateTime;
 /**
  *
  * @author stevedownes
  */
 public class Transaksi {
-    
+    public long hitungBiaya(Tiket tiket) {
+        LocalDateTime keluar = LocalDateTime.now();
+
+        long durasiMenit = Duration.between(
+                tiket.getWaktuMasuk(),
+                keluar
+        ).toMinutes();
+
+        long jam = (long) Math.ceil(durasiMenit / 60.0);
+
+        int tarif = tiket.getKendaraan().getTarifPerJam();
+
+        return jam * tarif;
+    }
 }
